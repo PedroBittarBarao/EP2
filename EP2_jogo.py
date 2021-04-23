bd0de24f42795dc9168f33d55c232799eb235205
Pedro Bittar Barão <pedrobaraocontas@gmail.com>
2021-04-23 14:39:39 -0300

    Código para imprimir baralho embaralhado

--- EP2_jogo.py
+++ EP2_jogo.py
@@ -1,2 +1,8 @@
 import EP2_funçoes
-baralho=random.sample(cria_baralho)
+import random
+baralho=EP2_funçoes.cria_baralho()
+baralho=random.sample(baralho,52)
+i=0
+while i<len(baralho):
+    print('{}: {}'.format(i+1,baralho[i]))
+    i+=1
